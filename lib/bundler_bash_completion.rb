9a61d82a73b0dedaa7b52f40348aab5cc02cf5a8
Alexis Toulotte <al@alweb.org>
2012-03-26 17:17:39 +1100

    added simple tasks completion

--- lib/bundler_bash_completion.rb
+++ lib/bundler_bash_completion.rb
@@ -23,8 +23,43 @@ class BundlerBashCompletion
     @line = line.to_s.gsub(/^\s+/, '').freeze
   end
   
+  def arguments
+    @arguments ||= line.split(/\s+/)
+  end
+  
+  def command
+    arguments.first.to_s
+  end
+  
   def complete
+    return tasks if task_completion?
     []
+  end
+  
+  private
+  
+  def available?
+    command == 'bundle' && completion_step > 0
+  end
+  
+  def completion_step
+    @completion_step ||= arguments.size - 1 + (line =~ /\s+$/ ? 1 : 0)
+  end
+  
+  def completion_step?(step)
+    completion_step == step
+  end
+  
+  def task
+    @task ||= arguments[1].to_s
+  end
+  
+  def task_completion?
+    available? && completion_step?(1)
+  end
+  
+  def tasks
+    TASKS.select { |t| t.start_with?(task) }
   end
   
 end
--- spec/bundler_bash_completion_spec.rb
+++ spec/bundler_bash_completion_spec.rb
@@ -2,34 +2,121 @@ require 'spec_helper'
 
 describe BundlerBashCompletion do
   
-  describe '#complete' do
+  def completion(line)
+    BundlerBashCompletion.new(line)
+  end
+  
+  describe '#arguments' do
 
-    it 'should have specs'
+    it 'is given line splitted by whitespaces' do
+      completion('bundle exec   rails s').arguments.should == ['bundle', 'exec', 'rails', 's']
+    end
+
+  end
+  
+  describe '#command' do
+
+    it 'is first argument' do
+      completion('bundle exec').command.should == 'bundle'
+      completion('rake').command.should == 'rake'
+    end
+    
+    it 'is empty string if line is blank' do
+      completion(nil).command.should == ''
+    end
+
+  end
+  
+  describe '#complete' do
+    
+    context 'with "foo"' do
+
+      it 'is an empty array' do
+        completion('foo').complete.should == []
+      end
+
+    end
+    
+    context 'with "bundle"' do
+
+      it 'is an empty array' do
+        completion('bundle').complete.should == []
+      end
+
+    end
+    
+    context 'with "bundle "' do
+
+      it 'is all tasks' do
+        completion('bundle ').complete.should include('check', 'outdated', 'list', 'show')
+      end
+
+    end
+    
+    context 'with "bundle  "' do
+
+      it 'is all tasks' do
+        completion('bundle  ').complete.should include('check', 'outdated', 'list', 'show')
+      end
+
+    end
+    
+    context 'with "bundle in"' do
+
+      it 'is "init" & "install" tasks' do
+        completion('bundle in').complete.should == ['init', 'install']
+      end
+
+    end
+    
+    context 'with "bundle in "' do
+
+      it 'is an empty array' do
+        completion('bundle in ').complete.should == []
+      end
+
+    end
+    
+    context 'with "bundle foo"' do
+
+      it 'returns an empty array' do
+        completion('bundle foo').complete.should == []
+      end
+
+    end
+    
+    context 'with "bundle foo "' do
+
+      it 'returns an empty array' do
+        completion('bundle foo ').complete.should == []
+      end
+
+    end
 
   end
 
   describe '#line' do
     
     it 'is line given at initializion' do
-      BundlerBashCompletion.new('hello').line.should == 'hello'
+      completion('hello').line.should == 'hello'
     end
 
     it 'is frozen' do
       expect {
-        BundlerBashCompletion.new('hello').line.gsub!('l', 'w')
+        completion('hello').line.gsub!('l', 'w')
       }.to raise_error(/can't modify frozen String/)
     end
     
     it 'is converted to string' do
-      BundlerBashCompletion.new(:hello).line.should == 'hello'
+      completion(:hello).line.should == 'hello'
     end
     
     it 'first whitespaces are removed' do
-      BundlerBashCompletion.new(' hello').line.should == 'hello'
+      completion(' hello').line.should == 'hello'
     end
     
     it 'last whitespaces are preserved' do
-      BundlerBashCompletion.new('hello ').line.should == 'hello '
+      completion('hello ').line.should == 'hello '
     end
 
   end
